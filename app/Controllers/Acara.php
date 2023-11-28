cee8cf71954316fada7c8159393d5624cd929329
zenlie <jenly.ramdan3199@gmail.com>
2023-11-28 12:17:57 +0700

    part 5 - query builder

--- app/Controllers/Acara.php
+++ app/Controllers/Acara.php
@@ -6,6 +6,12 @@ class Acara extends BaseController
 {
     public function index(): string
     {
-        return view('acara/index');
+        $builder = $this->db->table('e_wedding');
+        $query   = $builder->get()->getResult();
+        
+        // print_r($query);
+        // print_r($query->getResult());
+        $data['e_wedding'] = $query;
+        return view('acara/index', $data);
     }
 }
--- app/Views/acara/index.php
+++ app/Views/acara/index.php
@@ -1,13 +1,46 @@
 <?= $this->extend('layout/default') ?>
 
+<?= $this->section('title') ?>
+<title>Data Acara &mdash; E-Wedding</title>
+<?= $this->endSection() ?>
+
 <?= $this->section('content') ?>
     <section class="section">
         <div class="section-header">
-        <h1>Acara</h1>
+            <h1>Acara</h1>
         </div>
 
         <div class="section-body">
-            show data acara
+            <div class="card">
+                <div class="card-header">
+                    <h4>Data Acara</h4>
+                </div>
+                <div class="card-body table-responsive">
+                    <table class="table table-striped table-md">
+                        <tbody>
+                            <tr>
+                                <th>#</th>
+                                <th>Nama</th>
+                                <th>Tanggal</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                            <?php foreach ($e_wedding as $key => $value) : ?>
+                                <tr>
+                                    <td><?=$key + 1?></td>
+                                    <td><?=$value -> name?></td>
+                                    <td><?=$value -> date?></td>
+                                    <td><?=$value -> info?></td>
+                                    <td>
+                                        <a href="#" class="btn btn-warning btn-sm"><i class="fas fa-pencil-alt"></i></a>
+                                        <a href="#" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </section>
 <?= $this->endSection() ?>
